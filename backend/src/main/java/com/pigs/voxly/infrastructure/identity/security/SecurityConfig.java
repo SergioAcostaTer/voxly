341739a4de5decfbf457adbdfd09c6616848dbca
Sergio Acosta Quintana <sergioacostaquintana@gmail.com>
2026-03-10 19:07:35 +0000

    fix

--- backend/src/main/java/com/pigs/voxly/infrastructure/identity/security/SecurityConfig.java
+++ backend/src/main/java/com/pigs/voxly/infrastructure/identity/security/SecurityConfig.java
@@ -1,6 +1,5 @@
 package com.pigs.voxly.infrastructure.identity.security;
 
-import java.beans.Customizer;
 import java.nio.charset.StandardCharsets;
 import java.util.List;
 
@@ -9,6 +8,7 @@ import javax.crypto.spec.SecretKeySpec;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
